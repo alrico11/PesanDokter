189d94a4299019b5c46fc6a27e9f5f7526ad828e
alricowibowo <alricowibowo@gmail.com>
2024-01-05 13:31:41 +0700

    fix: admin & dokter panels

---
+++ pesan-dokter.sql
@@ -0,0 +1,524 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 05, 2024 at 07:31 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pesan-dokter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftar_polis`
+--
+
+CREATE TABLE `daftar_polis` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_pasien` bigint(20) UNSIGNED NOT NULL,
+  `id_jadwal` bigint(20) UNSIGNED NOT NULL,
+  `keluhan` text NOT NULL,
+  `no_antrian` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `daftar_polis`
+--
+
+INSERT INTO `daftar_polis` (`id`, `id_pasien`, `id_jadwal`, `keluhan`, `no_antrian`, `created_at`, `updated_at`) VALUES
+(34, 11, 1, 'sakit mata', 1, '2024-01-04 06:31:06', '2024-01-04 06:31:06'),
+(35, 10, 3, 'Rabun', 1, '2024-01-04 22:36:43', '2024-01-04 22:36:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_periksas`
+--
+
+CREATE TABLE `detail_periksas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_periksa` bigint(20) UNSIGNED NOT NULL,
+  `id_obat` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokters`
+--
+
+CREATE TABLE `dokters` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_hp` bigint(20) NOT NULL,
+  `id_poli` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `dokters`
+--
+
+INSERT INTO `dokters` (`id`, `nama`, `alamat`, `no_hp`, `id_poli`, `created_at`, `updated_at`) VALUES
+(1, 'Laksono', 'Purwoyoso', 85290328541, 1, '2023-12-25 00:17:14', '2023-12-25 00:36:13'),
+(2, 'Suryanto', 'Kembang arum', 8522222222, 3, '2024-01-04 20:49:50', '2024-01-04 20:49:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal_periksas`
+--
+
+CREATE TABLE `jadwal_periksas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_dokter` bigint(20) UNSIGNED NOT NULL,
+  `hari` enum('senin','selasa','rabu','kamis','jumat','sabtu') NOT NULL,
+  `jam_mulai` time NOT NULL,
+  `jam_selesai` time NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `jadwal_periksas`
+--
+
+INSERT INTO `jadwal_periksas` (`id`, `id_dokter`, `hari`, `jam_mulai`, `jam_selesai`, `created_at`, `updated_at`) VALUES
+(1, 1, 'senin', '14:04:59', '17:04:59', '2023-12-14 01:04:59', '2023-12-28 01:04:59'),
+(2, 1, 'rabu', '00:00:00', '12:30:00', '2023-12-25 18:13:57', '2023-12-25 18:13:57'),
+(3, 1, 'rabu', '19:15:37', '19:15:36', '2024-01-04 05:15:38', '2024-01-04 05:15:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_12_19_180000_create_poli_table', 1),
+(6, '2023_12_19_180622_create_dokter_table', 1),
+(7, '2023_12_19_180623_create_obat_table', 1),
+(8, '2023_12_19_180624_create_pasien_table', 1),
+(9, '2023_12_19_181439_create_jadwal_periksa_table', 1),
+(10, '2023_12_19_181740_create_daftar_poli_table', 1),
+(11, '2023_12_19_181741_create_periksa_table', 1),
+(12, '2023_12_19_181742_create_detail_periksa_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obats`
+--
+
+CREATE TABLE `obats` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_obat` varchar(255) NOT NULL,
+  `kemasan` varchar(255) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `obats`
+--
+
+INSERT INTO `obats` (`id`, `nama_obat`, `kemasan`, `harga`, `created_at`, `updated_at`) VALUES
+(1, 'Demacolin', 'Tablet', 5000, '2023-12-26 19:54:35', '2023-12-26 19:54:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasiens`
+--
+
+CREATE TABLE `pasiens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_ktp` varchar(255) NOT NULL,
+  `no_hp` varchar(255) NOT NULL,
+  `no_rm` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pasiens`
+--
+
+INSERT INTO `pasiens` (`id`, `nama`, `alamat`, `no_ktp`, `no_hp`, `no_rm`, `created_at`, `updated_at`) VALUES
+(10, 'Alrico Rizki Wibowo', '123', '123', '085290328542', '202401-001', '2024-01-04 01:55:21', '2024-01-04 22:32:59'),
+(11, 'Alrico Rizki Wibowo', 'Jl. Purwoyoso RT 07 RW 12', '123123123', '085290328542', '202401-002', '2024-01-04 06:30:48', '2024-01-04 06:30:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `periksas`
+--
+
+CREATE TABLE `periksas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_daftar_poli` bigint(20) UNSIGNED NOT NULL,
+  `tgl_periksa` date NOT NULL,
+  `catatan` text NOT NULL,
+  `biaya_periksa` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `periksas`
+--
+
+INSERT INTO `periksas` (`id`, `id_daftar_poli`, `tgl_periksa`, `catatan`, `biaya_periksa`, `created_at`, `updated_at`) VALUES
+(4, 34, '2024-01-04', 'Sakit picek', 155000, '2024-01-04 09:05:18', '2024-01-04 09:05:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `polis`
+--
+
+CREATE TABLE `polis` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_poli` varchar(255) NOT NULL,
+  `keterangan` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `polis`
+--
+
+INSERT INTO `polis` (`id`, `nama_poli`, `keterangan`, `created_at`, `updated_at`) VALUES
+(1, 'Anak', 'Dokter pengecekan anak', '2023-12-26 12:30:26', '2023-12-26 12:30:26'),
+(3, 'Jantung', 'Spesialis penyakit jantung', '2024-01-04 20:49:26', '2024-01-04 20:49:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `is_admin` tinyint(1) DEFAULT NULL,
+  `is_dokter` tinyint(1) DEFAULT NULL,
+  `id_dokter` bigint(20) UNSIGNED DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `is_admin`, `is_dokter`, `id_dokter`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'ADMIN', 'admin@admin.com', NULL, '$2y$12$EJSDWReEi7MhuQeisEju1Of.PmP7/BN3ablY9DgAwpB4B.TXgAwGe', 1, 0, NULL, NULL, '2023-12-19 11:34:53', '2023-12-19 11:34:53'),
+(2, 'DOKTER', 'admin1@admin.com', NULL, '$2y$12$EJSDWReEi7MhuQeisEju1Of.PmP7/BN3ablY9DgAwpB4B.TXgAwGe', 0, 1, 1, NULL, '2023-12-19 11:34:53', '2023-12-19 11:34:53'),
+(4, 'Suryanto', 'yanto@admin.com', NULL, '$2y$12$1.LUqd0gBz3/I2wAqUbXkuYEWmy6j/0Wis0qRAZtzQU.k2G1g5g7y', 0, 1, 2, NULL, '2024-01-04 20:51:28', '2024-01-04 20:51:28');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `daftar_polis`
+--
+ALTER TABLE `daftar_polis`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `daftar_polis_id_pasien_foreign` (`id_pasien`),
+  ADD KEY `daftar_polis_id_jadwal_foreign` (`id_jadwal`);
+
+--
+-- Indexes for table `detail_periksas`
+--
+ALTER TABLE `detail_periksas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `detail_periksas_id_periksa_foreign` (`id_periksa`),
+  ADD KEY `detail_periksas_id_obat_foreign` (`id_obat`);
+
+--
+-- Indexes for table `dokters`
+--
+ALTER TABLE `dokters`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dokters_id_poli_foreign` (`id_poli`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `jadwal_periksas`
+--
+ALTER TABLE `jadwal_periksas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jadwal_periksas_id_dokter_foreign` (`id_dokter`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `obats`
+--
+ALTER TABLE `obats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pasiens`
+--
+ALTER TABLE `pasiens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `periksas`
+--
+ALTER TABLE `periksas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `periksas_id_daftar_poli_foreign` (`id_daftar_poli`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `polis`
+--
+ALTER TABLE `polis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD KEY `users_id_dokter_foreign` (`id_dokter`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `daftar_polis`
+--
+ALTER TABLE `daftar_polis`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `detail_periksas`
+--
+ALTER TABLE `detail_periksas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `dokters`
+--
+ALTER TABLE `dokters`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jadwal_periksas`
+--
+ALTER TABLE `jadwal_periksas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `obats`
+--
+ALTER TABLE `obats`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pasiens`
+--
+ALTER TABLE `pasiens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `periksas`
+--
+ALTER TABLE `periksas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `polis`
+--
+ALTER TABLE `polis`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `daftar_polis`
+--
+ALTER TABLE `daftar_polis`
+  ADD CONSTRAINT `daftar_polis_id_jadwal_foreign` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal_periksas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `daftar_polis_id_pasien_foreign` FOREIGN KEY (`id_pasien`) REFERENCES `pasiens` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `detail_periksas`
+--
+ALTER TABLE `detail_periksas`
+  ADD CONSTRAINT `detail_periksas_id_obat_foreign` FOREIGN KEY (`id_obat`) REFERENCES `obats` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detail_periksas_id_periksa_foreign` FOREIGN KEY (`id_periksa`) REFERENCES `periksas` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `dokters`
+--
+ALTER TABLE `dokters`
+  ADD CONSTRAINT `dokters_id_poli_foreign` FOREIGN KEY (`id_poli`) REFERENCES `polis` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `jadwal_periksas`
+--
+ALTER TABLE `jadwal_periksas`
+  ADD CONSTRAINT `jadwal_periksas_id_dokter_foreign` FOREIGN KEY (`id_dokter`) REFERENCES `dokters` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `periksas`
+--
+ALTER TABLE `periksas`
+  ADD CONSTRAINT `periksas_id_daftar_poli_foreign` FOREIGN KEY (`id_daftar_poli`) REFERENCES `daftar_polis` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_id_dokter_foreign` FOREIGN KEY (`id_dokter`) REFERENCES `dokters` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
